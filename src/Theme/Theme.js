6f0f3e29d483c66e6f3e9640e91696e4eaf1aa84
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-01-12 21:01:53 +0200

    Create Theme.js

---
+++ src/Theme/Theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    textPrimary: 'rgb(0, 0, 0)',
+    secondText: 'rgb(255, 255, 255)',
+    grayText: 'rgb(123, 123, 123)',
+    bcPrimary: 'rgb(255, 255, 255)',
+    bcBtn: 'rgb(0, 0, 225)',
+    delBtn: 'rgb(255, 0, 0)',
+  },
+  fonts: {
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'Georgia, serif',
+    monospace: 'Menlo, monospace',
+    operatorMono: 'Operator Mono',
+    georgia: ' Georgia',
+    arial: 'Arial',
+    gillSans: 'Gill Sans',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+
+  borders: {
+    border: '0.5px solid rgb(216, 214, 214)',
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+
+  shadow: {
+    boxShadow: '0px 16px 50px -3px rgba(0,0,0,0.6)',
+  },
+};
